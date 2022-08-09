e2ea2ec11b37043750a58de6de46e479e4a5bf93
Yakshesh Gupta <98878754+yaksheshgupta@users.noreply.github.com>
2022-08-09 15:50:29 +0530

    Update 2_deploy_token_factory.js

--- contracts/migrations/2_deploy_token_factory.js
+++ contracts/migrations/2_deploy_token_factory.js
@@ -1,5 +1,6 @@
 const tokenFactory = artifacts.require("TokenFactory");
-
+const ERC20Token = artifacts.require("ERC20Token");
 module.exports = function (deployer) {
   deployer.deploy(tokenFactory);
+  deployer.deploy(ERC20Token,"Yakshesh","YAKSH",100,"0x55be5FB979fe19184c0Ccd92679876416a84BBcE")
 };
