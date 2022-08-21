57439125f5c20ef51be6c1c877901b277a2d42c7
Yakshesh Gupta <yaksheshgupta59@gmail.com>
2022-08-21 15:25:18 +0530

    ivotes

--- contracts/migrations/2_deploy_token_factory.js
+++ contracts/migrations/2_deploy_token_factory.js
@@ -2,10 +2,20 @@ const tokenFactory = artifacts.require("TokenFactory");
 const ERC20Token = artifacts.require("ERC20Token");
 const crowdsale = artifacts.require("Crowdsale");
 const crowdsaleFactory = artifacts.require("CrowdsaleFactory");
-module.exports = function (deployer) {
+const ivote=artifacts.require("IVotes");
+const timelockController=artifacts.require("TimelockController");
+const mygo=artifacts.require("MyGovernor");
+const proposers= ["0x55be5FB979fe19184c0Ccd92679876416a84BBcE"];
+const executors= ["0x313aEB130dB7879212Ce6b19c5d3B3b173b53D52"];
+module.exports =  (deployer)=> {
   deployer.deploy(tokenFactory);
   deployer.deploy(ERC20Token,"Yakshesh","YAKSH",10,"0x55be5FB979fe19184c0Ccd92679876416a84BBcE").then(()=>{
     return deployer.deploy(crowdsale,1,"0x55be5FB979fe19184c0Ccd92679876416a84BBcE",ERC20Token.address)
   })
   deployer.deploy(crowdsaleFactory);
+  deployer.deploy(ivote).then(()=>{
+   return deployer.deploy(timelockController,11,proposers,executors).then(()=>{
+     return deployer.deploy(mygo,ivote.address,timelockController.address);
+   })
+  })
 };
