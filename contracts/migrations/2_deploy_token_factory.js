7cef30da72c9b45e3a65677529e5b427d983e091
Yakshesh Gupta <98878754+yaksheshgupta@users.noreply.github.com>
2022-09-22 18:41:25 +0530

    Update 2_deploy_token_factory.js

--- contracts/migrations/2_deploy_token_factory.js
+++ contracts/migrations/2_deploy_token_factory.js
@@ -4,16 +4,18 @@ const crowdsale = artifacts.require("Crowdsale");
 const crowdsaleFactory = artifacts.require("CrowdsaleFactory");
 const timelockController=artifacts.require("TimelockController");
 const mygo=artifacts.require("MyGovernor");
-const proposers= ["0x55be5FB979fe19184c0Ccd92679876416a84BBcE"];
-const executors= ["0x313aEB130dB7879212Ce6b19c5d3B3b173b53D52"];
+const proposers= ["0x5924a98CEE7f4a9360Dfc848eFf55CAC2591Ca2A"];
+const executors= ["0x451d77133Ce919aBAbd4E303B223E82ea5185154"];
+const owner="0x35539baD7929a07772EE0cb101e7778143A21FE9"
+
+
 module.exports =  (deployer)=> {
   deployer.deploy(tokenFactory);
-  deployer.deploy(ERC20Token,"Yakshesh","YAKSH",1000,"0x55be5FB979fe19184c0Ccd92679876416a84BBcE").then(()=>{
-    return deployer.deploy(crowdsale,1,"0x55be5FB979fe19184c0Ccd92679876416a84BBcE",ERC20Token.address)
+  deployer.deploy(ERC20Token,"Yakshesh","YAKSH",100,owner).then(()=>{
+    return deployer.deploy(crowdsale,1,owner,ERC20Token.address)
   })
   deployer.deploy(crowdsaleFactory);
   deployer.deploy(timelockController,11,proposers,executors).then(()=>{
      return deployer.deploy(mygo,ERC20Token.address,timelockController.address);
    })
-  // console.log(ERC20Token);
 };
